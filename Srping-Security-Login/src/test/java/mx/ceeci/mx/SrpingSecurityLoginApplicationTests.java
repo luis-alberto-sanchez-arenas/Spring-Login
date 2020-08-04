a9101cdaf09d6a21681277837fa185f064e9aa0e
Luis Alberto Sanchez Arenas <luis.mpc38@gmail.com>
2020-08-03 20:23:21 -0500

    commit inicial con eclipse

---
+++ Srping-Security-Login/src/test/java/mx/ceeci/mx/SrpingSecurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ceeci.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingSecurityLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
