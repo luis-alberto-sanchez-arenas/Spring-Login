3ea246bd1b8415d7bc6e6f4fb6560b0b50501168
Luis Alberto Sanchez Arenas <luis.mpc38@gmail.com>
2020-08-03 20:23:21 -0500

    commit inicial con eclipse. Close #1

---
+++ Srping-Security-Login/src/main/java/mx/ceeci/mx/SrpingSecurityLoginApplication.java
@@ -0,0 +1,13 @@
+package mx.ceeci.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingSecurityLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingSecurityLoginApplication.class, args);
+	}
+
+}
